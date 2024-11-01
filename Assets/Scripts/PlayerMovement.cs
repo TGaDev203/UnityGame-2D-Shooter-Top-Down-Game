4b8df419a2317e6c49880725023c168da1412ad8
TGaDev <hoangthuychimte@gmail.com>
2024-11-01 10:59:51 +0700

    Add Basic Movement And Optimize Performance

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,31 +2,66 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //! Components
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float steerSpeed;
+    [SerializeField] private Camera mainCamera;
+
     private Rigidbody2D playerRigidbody2D;
+    private Vector2 movement;
+    private Vector3 lastMousePos;
 
     private void Awake()
-    {
-        InitializeComponents();
-    }
-
-    private void InitializeComponents()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        HandlePlayerMovement();
+        CheckStartGame();
+        UpdateMovementInput();
     }
 
-    private void HandlePlayerMovement()
+    private void FixedUpdate()
     {
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        transform.Translate(0, moveAmount, 0);
-        transform.Rotate(0, 0, -steerAmount);
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    private void UpdateMovementInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        Vector2 newPosition = playerRigidbody2D.position + movement * moveSpeed * Time.fixedDeltaTime;
+        playerRigidbody2D.MovePosition(newPosition);
+    }
+
+    private void RotatePlayer()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        // Only update rotation if mouse has moved significantly
+        if ((mousePos - lastMousePos).sqrMagnitude > 0.1f)
+        {
+            Vector3 worldMousePos = mainCamera.ScreenToWorldPoint(mousePos);
+            worldMousePos.z = 0f;
+
+            Vector2 direction = (worldMousePos - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            playerRigidbody2D.rotation = angle;
+
+            lastMousePos = mousePos;
+        }
+    }
+
+    private void CheckStartGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Cursor.visible = false;
+            // Cursor.lockState = Cursor.visible ? CursorLockMode.None : CursorLockMode.Locked;
+        }
     }
 }
